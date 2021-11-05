959e472a7ccecb8cea5c8d9020c0c98b6fc8767e
Bisshoy <42306585+Bisshoy@users.noreply.github.com>
2021-11-05 11:53:52 -0400

    Update CaesarPlusPlus.cpp

--- CaesarPlusPlus.cpp
+++ CaesarPlusPlus.cpp
@@ -1,22 +1,3 @@
-/*
-This is a program that allows the user to input a string of any length. 
-The user will also choose a shift value. The program will shift the 
-letters of the user's original sentence by a number equal to the shift 
-value. For example, if the entered message is abcde and the shift value
-is 2, then the cyphered text is cdefg. 
-
-This program also includes a decypher option; for this, input the same 
-shift value as the original; no extra work needs to be done. If the 
-original text was cyphered with a shift value of 5, then to decypher it,
-the user should input 5 at the prompt as well. 
-
-[NOTE: The formula for the decode shift is: 26 - (shift % 26)]
-
-@version 11-5-2021 (Happy Guy Fawkes Day!)
-@author Bisshoy Anwar 
-*/
-
-
 #include <iostream>
 
 // List of prototypes 
